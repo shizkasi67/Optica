da239c521a7db0d3077eed710131d529f395e8ae
Luciano Lopez <lucianozx29@gmail.com>
2025-08-31 16:39:37 -0400

    Add files via upload

---
+++ WebOptica/productos.js
@@ -0,0 +1,77 @@
+function cambiarImagen(element) {
+    element.src = "producto1.1.jpeg";
+  }
+  
+  function restaurarImagen(element) {
+    element.src = "producto1.jpeg";
+  }
+
+
+
+function cambiarImagen2(element) {
+    element.src = "producto2.2.jpeg";
+}
+
+function restaurarImagen2(element) {
+    element.src = "producto2.jpeg";
+  }
+
+  function cambiarImagen3(element) {
+    element.src = "producto3.3.jpeg";
+}
+
+function restaurarImagen3(element) {
+    element.src = "producto3.jpeg";
+  }
+
+  function cambiarImagen4(element) {
+    element.src = "producto4.4.jpeg";
+}
+
+function restaurarImagen4(element) {
+    element.src = "producto4.jpeg";
+  }
+
+function restaurarImagen5(element) {
+    element.src = "producto5.jpeg";
+  }
+
+  function cambiarImagen5(element) {
+    element.src = "producto5.5.jpeg";
+}
+
+function restaurarImagen6(element) {
+    element.src = "producto6.jpeg";
+  }
+
+  function cambiarImagen6(element) {
+    element.src = "producto6.6.jpeg";
+}
+
+function restaurarImagen6(element) {
+    element.src = "producto6.jpeg";
+  }
+
+  function cambiarImagen7(element) {
+    element.src = "producto7.7.jpeg";
+}
+
+function restaurarImagen7(element) {
+    element.src = "producto7.jpeg";
+  }
+
+  function cambiarImagen8(element) {
+    element.src = "producto8.8.jpeg";
+}
+
+function restaurarImagen8(element) {
+    element.src = "producto8.jpeg";
+  }
+
+  function cambiarImagen9(element) {
+    element.src = "producto9.9.jpeg";
+}
+
+function restaurarImagen9(element) {
+    element.src = "producto9.jpeg";
+  }
